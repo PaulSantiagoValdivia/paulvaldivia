5ea2ca1efb72ce065be5dc10d769347ab300b92e
santiago <paulsantiagovaldivia@gmail.com>
2023-06-25 20:55:54 -0400

    add: styles desktop

--- components/main/Main.js
+++ components/main/Main.js
@@ -15,13 +15,12 @@ export default function Main() {
         <p className={styles.presentation}>
           <span className={styles.hola}>Hola! 👋</span>, Soy Santiago y me gusta escribir codigo.
         </p>
-        <p className={styles.tecnologies}>
-          Mi stack favorito es React usando Next.js con styled components <br />
+        <p className={styles.tecnologies}>Mi stack favorito es React usando Next.js con styled components
           y supabase como base de datos.
         </p>
         <button className={styles.ancor}>Ver mis proyectos <Icon /></button>
         <div className={styles.banner}>
-          <img src="/vector.png" alt="Imagen vector" />
+          <img className={styles.img2} src="/vector.png" alt="Imagen vector" />
         </div>
         <div className={styles.carrusel}>
           <img className={styles.img} src="/carrusel.png" alt="Imagen carrusel" />
